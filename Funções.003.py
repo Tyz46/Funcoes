73791acdc8c57d398d17598c1c6fb375d2e09dfa
Tyz46 <thomas.bruz@pucpr.edu.br>
2025-05-20 09:22:33 -0300

    SomaFunc

---
+++ Funções.003.py
@@ -0,0 +1,9 @@
+num1 = int(input('Escolha um número: '))
+num2 = int(input('Escolha um número: '))
+num3 = int(input('Escolha um número: '))
+
+
+def soma(num1, num2, num3):
+    soma1 = num1 + num2 + num3
+    print(f'O resultado da soma é, {soma1}')
+soma()
